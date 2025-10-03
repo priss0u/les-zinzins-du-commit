0191934f6759b575272a3bb6633e0b2eb720a5a9
priss0u <mandozipriscille@gmail.com>
2025-10-03 10:21:11 +0200

    fin ticket commentaire

--- src/Controllers/CommitController.php
+++ src/Controllers/CommitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\Comment;
 use App\Models\Commit;
 use App\Utils\AbstractController;
 
@@ -39,6 +40,17 @@ class CommitController extends AbstractController
 
             if($myCommit)
             {
+                if(isset($_POST['addComment'])){
+                    $text = htmlspecialchars($_POST['comment']);
+                    $this->totalCheck('comment', $text);
+                    if(empty($this->arrayError)){
+                        $today = date("Y-m-d");
+                        $comment = new Comment(null, $text, $today, null, $id, $_SESSION['user']['id_user']);
+                        $comment->addComment();
+                        $this->redirectToRoute('/commit?id=' . $id, 200);
+                    }
+                }
+
                 require_once(__DIR__ . "/../Views/commit.view.php");
             }else{
                 $this->redirectToRoute('/', 302);
@@ -46,5 +58,36 @@ class CommitController extends AbstractController
         }else{
             $this->redirectToRoute('/', 302);
         }
+    }
+
+    public function editCommit()
+    {
+        if(isset($_GET['id'])){
+            $id = htmlspecialchars($_GET['id']);
+            $commit = new Commit($id, null, null, null, null, null, null, null, null, null, null);
+            $myCommit = $commit->getCommitById();
+            
+            if($myCommit && ($_SESSION['user']['id_user'] === $myCommit->getUserId())){
+
+                if(isset($_POST['editCommit'])){
+                    $text = htmlspecialchars($_POST['commit']);
+                    $this->totalCheck('commit', $text);
+
+                    if(empty($this->arrayError)){
+                        $today = date("Y-m-d");
+                        $updateCommit = new Commit($id, $text, null, $today, null, null, null, null, null, null);
+                        $updateCommit->editCommit();
+                        $this->redirectToRoute('/commit?id='.$id , 200);
+                    }
+                }
+
+                require_once(__DIR__ . "/../Views/editCommit.view.php");
+            }else{
+                $this->redirectToRoute('/', 302);
+            }
+        }else{
+            $this->redirectToRoute('/', 302);
+        }
+        
     }
 }
--- src/Views/commit.view.php
+++ src/Views/commit.view.php
@@ -4,6 +4,8 @@ require_once(__DIR__ . "/partials/head.view.php");
 <h1>Un commit</h1>
 <p><?= $myCommit->getText(); ?></p>
 <p>Date de création <?= $myCommit->getCreationDate();?></p>
+
+
 <?php
     if($myCommit->getModificationDate()){
         ?>
@@ -11,6 +13,34 @@ require_once(__DIR__ . "/partials/head.view.php");
         <?php
     }
 ?>
-<?php echo  $myCommit->getModificationDate() ? "<p>Date de modification " . $myCommit->getModificationDate() . "</p>" : "" ?>
+
+<?php
+    if(isset($_SESSION['user']) && $_SESSION['user']['id_user'] === $myCommit->getUserId()){
+?>
+    <a href="/modifier?id=<?= $myCommit->getIdCommit();?>" class="btn btn-warning">Modifier</a>
+<?php
+    }
+    if(isset($_SESSION['user'])){
+    ?>
+        <form method="POST">
+            <div class="container">
+                <div class="form-group">
+                    <label for="comment" class="form-label">Quelque chose à dire ?</label>
+                    <textarea class="form-control" id="comment" name="comment" style="height: 100px"></textarea>
+                    <?php
+                    if(isset($this->arrayError['comment'])){
+                        ?>
+                        <p class="text-danger"><?= $this->arrayError['comment']?></p>
+                        <?php
+                    }
+                    ?>
+                </div>
+                <button type="submit" name="addComment" class="btn btn-success">Commenter !</button>
+            </div>
+        </form>
+    <?php
+    }
+?>
+
 <?php
 require_once(__DIR__ . "/partials/footer.view.php");
